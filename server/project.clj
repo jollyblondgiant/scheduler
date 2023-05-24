9c8e35b33f3991a7111a060c3e1634362a5eed48
Andrew Carlile <andrew.carlil3@gmail.com>
2023-05-23 21:13:42 -0400

    basic server setup

--- server/project.clj
+++ server/project.clj
@@ -1,12 +1,25 @@
 (defproject server "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler server.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                 ;;
+                 [ring "1.10.0"]
+                 [compojure "1.7.0"]
+                 [metosin/reitit "0.7.0-alpha4"]
+
+                 [cheshire "5.11.0"]
+
+
+                 [http-kit "2.7.0-beta3"]
+
+                 [datascript "1.4.2"]
+
+                 [prismatic/schema "1.4.1"]
+
+                 [clj-time "0.15.2"]]
+  :main server.main
+  :profiles {:dev 
+             {:main server.main/-dev-main}}
+  :repl-options {:init-ns server.core})
